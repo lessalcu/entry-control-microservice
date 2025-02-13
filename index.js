700de7f048805b92004afafbd48e33e7a9ee0a16
Salomé Salas <154089468+lessalcu@users.noreply.github.com>
2025-02-12 21:16:22 -0500

    feat: :sparkles:

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); 
 const entryRoutes = require('./src/routes/entryRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 7000;
 
+app.use(cors());  
+
 app.use(express.json());
-app.use('/entry', entryRoutes);
+app.use('/entry', entryRoutes); 
 
 app.listen(PORT, () => {
     console.log(`Entry Control Service running on port ${PORT}`);
